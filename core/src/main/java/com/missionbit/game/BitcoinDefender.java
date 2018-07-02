b1113976e9b554e5c137a5b32e523d6e822ebe85
SaltMeister <huisimonliu@gmail.com>
2018-07-02 11:22:32 -0700

    added a 2d camera in 800 x 480

--- core/src/main/java/com/missionbit/game/BitcoinDefender.java
+++ core/src/main/java/com/missionbit/game/BitcoinDefender.java
@@ -1,7 +1,33 @@
 package com.missionbit.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class BitcoinDefender extends ApplicationAdapter {
+    private OrthographicCamera camera;
+    private Random randomSource;
+    private Sprite myImage;
+    private SpriteBatch myBatch;
+    private Vector2 velocity;
+
+    @Override
+    public void create() {
+        randomSource = new Random();
+
+        // Set up camera for 2d view of 800x480 pixels
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+        // Create a sprite batch for rendering our image
+        myBatch = new SpriteBatch();
+
+        //TODO: Load our image
+    }
+
 }
